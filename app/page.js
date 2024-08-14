d34879fd4f48872d2b40adf4cddcea7248a4db4a
Женя Готовец <phpjsmaster@gmail.com>
2024-08-14 17:33:08 +0300

    13

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js - OK 12!</code>
+          <code className={styles.code}>app/page.js - OK 13!</code>
         </p>
         <div>
           <a
